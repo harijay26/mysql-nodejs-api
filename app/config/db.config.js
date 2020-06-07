33c92c0a2f90571e494326a22f1ca51d9681852d
Promise Harry <chinecherempharry@gmail.com>
2020-06-07 17:43:05 +0200

    deploy again

---
+++ app/config/db.config.js
@@ -0,0 +1,21 @@
+
+//Configuration
+/* module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "12345",
+    DB: "webooks"
+}; */
+
+module.exports = {
+    HOST: "us-cdbr-east-05.cleardb.net",
+    USER: "bec1f5770c0621",
+    PASSWORD: "03bd5d7d",
+    DB: "heroku_72efa3630dadfaf"
+};
+
+//mysql://bec1f5770c0621:03bd5d7d@us-cdbr-east-05.cleardb.net/heroku_72efa3630dadfaf?reconnect=true
+
+//mysql --host=us-cdbr-east-05.cleardb.net --user=bec1f5770c0621 --password=03bd5d7d --reconnect heroku_72efa3630dadfaf
+
+

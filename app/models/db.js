fa0eb1f5dfb596ebc633cd9391ce251b0c37524b
Promise Harry <chinecherempharry@gmail.com>
2020-06-08 12:47:17 +0200

    deploy

--- app/models/db.js
+++ app/models/db.js
@@ -12,10 +12,10 @@ const connection = mysql.createPool({
 });
 
 // open the MySQL connection
-connection.connect(error => {
+/* connection.connect(error => {
     if (error) throw  error;
     console.log("Successfully connected to the database.");
-});
+}); */
 
 
 module.exports = connection;
